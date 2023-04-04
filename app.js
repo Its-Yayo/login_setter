2a97a11087322c6ca922a5293a4f4e2ad14bd677
a01754574 <a01754574@tec.mx>
2023-04-03 22:10:58 -0600

    Update app use #4

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').Server(app);
+
+// Path: routes/index.js
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    res.status(200);
+});
+
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+});
