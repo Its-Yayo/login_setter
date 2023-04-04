f8e38d9d0baa553c1ea695fb4993e0b2346d1c72
Its-Yayo <a01754574@gmail.com>
2023-04-03 22:29:00 -0600

    Update App Use Localhost #6

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const server = require('http').Server(app);
 
 // Path: routes/index.js
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+const app = express();
+const port = 8000;
+const ip = '127.0.0.1';
 
-server.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
     res.status(200);
+
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log('Server listening at port %d', port);
 });
