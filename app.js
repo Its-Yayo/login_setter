281a696ee73370a785ca0d5258f41b05a92c0aae
Its-Yayo <a01754574@gmail.com>
2023-04-03 22:29:42 -0600

    Update App Use Localhost #6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
     res.status(200);
-
+    res.type('html');
 });
 
 app.listen(port, () => {
