322e9f6fc023fd8c605e6d5da497c8cbc4a63e32
Its-yayo <a01754574@tec.mx>
2023-04-04 19:32:04 -0600

    Update API object fix #10

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-// Path: routes/main.js
+// Path: public/main.js
 const app = express();
 const port = 8000;
 const ip = '127.0.0.1';
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', (req, res) => {
+    res.send('API is working');
+    res.status(200);
+    res.type('html');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
